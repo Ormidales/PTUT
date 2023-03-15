f81794b3f690cb8121433087872f8548e57e6af5
nergizer <86924836+nergizer@users.noreply.github.com>
2023-03-15 17:58:41 +0100

    piou piou

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,18 +15,21 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();    
+        body = GetComponent<Rigidbody2D>();
     }
 
-    void OnCollisionEnter() {
-
+    void OnTriggerEnter2D(Collider2D collision) {
+        Destroy(transform.GetChild(0).gameObject);
+        Destroy(this.gameObject);
+        collision.gameObject.GetComponent<Entity>().Damage(5,null);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        var vec = (Quaternion.Euler(0,0,gameObject.transform.rotation.z) * new Vector2(0,Speed));
+        var vec = Quaternion.Euler(0,0,gameObject.transform.rotation.z) * new Vector2(0,Speed);
+        print(gameObject.transform.rotation.z);
         body.MovePosition(body.position + new Vector2(vec.x,vec.y));
     }
 }
