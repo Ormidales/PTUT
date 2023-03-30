4c1a5dffc994016132663899a39f4bd70acdf9fc
Leushuis <72081603+Leushuis@users.noreply.github.com>
2023-03-30 14:54:04 +0200

    shop

--- Assets/scripts/ShopManager.cs
+++ Assets/scripts/ShopManager.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
-    public Camera camera;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+    public GameObject object4;
+    public GameObject object5;
+    // Start is called before the first frame update
+    void Start()
     {
-        if(collision.CompareTag("Player"))
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
         {
-           Debug.Log("test");
+            ShopEnter.instance.closeShop();
+        }
+    }
+    public void sellObject1(int price)
+    {
+        if(Toins.instance.getToins()>=price)
+        {
+            Toins.instance.setToins(Toins.instance.getToins()-price);
+            object1.SetActive(false);
+        }
+    }
+    public void sellObject2(int price)
+    {
+        if(Toins.instance.getToins()>=price)
+        {
+            Toins.instance.setToins(Toins.instance.getToins()-price);
+            object2.SetActive(false);
         }
     }
 }
--- Assets/scripts/Toins.cs
+++ Assets/scripts/Toins.cs
@@ -8,10 +8,21 @@ public class Toins : MonoBehaviour
     public Text MyScoreText;
     private int ScoreNum;
 
+    public static Toins instance;
+    
+    private void Awake()
+    {
+        if (instance!=null)
+        {
+            Debug.LogWarning("Only one instance of Toins is expected !");
+            return;
+        }
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        ScoreNum = 0;
+        ScoreNum = 50;
         MyScoreText.text = ScoreNum.ToString();
     }
 
@@ -23,5 +34,14 @@ public class Toins : MonoBehaviour
             Destroy(Coin.gameObject); 
             MyScoreText.text = ScoreNum.ToString();
         }
+    }
+    public int getToins()
+    {
+        return ScoreNum;
+    }
+    public void setToins(int toins)
+    {
+        ScoreNum=toins;
+        MyScoreText.text = ScoreNum.ToString();
     }
 }
